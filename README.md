de89cf781dd2412189c9f0c005fb35fd5d77aec0
Lanner Silva <125982000+LannerSilva@users.noreply.github.com>
2023-04-08 12:39:47 -0300

    Update and rename README.md to modularizacao.c

--- README.md
+++
@@ -1,2 +0,0 @@
-# modulariacao
-materia da faculdade 
---
+++ modularizacao.c
@@ -0,0 +1,31 @@
+# modulariacao
+materia da faculdade 
+#include<stdio.h>
+
+void v_iniciacao(int *vet, int n) {
+int i;
+for(i=0; i<n; i++)
+vet[i]=0;
+}
+void v_imprime(int *vet, int n){
+    int i;
+    for(i=0; i < n; i++)
+    printf("%d - ",vet[i]);
+    printf("\n");
+}
+int main(void){
+int vet[10], i;
+v_iniciacao(vet,10);
+// impressao apos inicilialização.
+printf("Impressao do vetor antes da atribuicao.");
+v_imprime(vet,10);
+// Mudando valores de vet.
+for(i=0; i<10; i++){
+    vet[i]=i;
+}
+//Impressao apos a atribuição.
+printf("Impressao do vetor apos a atribuicao.");
+v_imprime(vet,10);
+return(0);
+
+}
